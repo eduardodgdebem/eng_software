db6a4ffc7b79c81e0e2ebe65eccb2006132a2e7c
Guilherme Fernandes <guifernandes111@gmail.com>
2024-04-03 20:17:42 -0300

    Update regostroEmprestimo.ts
    Atualizando o registro de emprestimo

--- eng_software/regostroEmprestimo.ts
+++ eng_software/regostroEmprestimo.ts
@@ -4,9 +4,13 @@ import { Emprestimo } from "./emprestimo";
 
 export class RegistroEmprestimo {
     public emprestimos: Emprestimo[]
-
+    
     public empresta(amigo: Amigo, dvd: DVD) {}
+    
     public devolve(dvd: DVD) {}
+    
     private estahEmprestado(dvd: DVD) {}
+    
     private faixaEtariaAbaixo(amigo: Amigo, dvd: DVD) {}
+    
 }
